d567f9da92f6dd8a96d0f038e9465f122feedf26
Oliver Schonrock <oliver@schonrocks.com>
2024-12-10 07:59:18 +0000

    pulling out helpers
    which are not relevant to usage

---
+++ test/helpers.hpp
@@ -0,0 +1,31 @@
+#include <charconv>
+#include <cstdint>
+#include <fstream>
+#include <random>
+#include <vector>
+
+std::vector<std::uint64_t> load_sample() {
+  std::ifstream              sample("data/sample.txt");
+  std::vector<std::uint64_t> keys;
+  for (std::string line; std::getline(sample, line);) {
+    std::uint64_t key{};
+    std::from_chars(line.data(), line.data() + line.size(), key, 16);
+    keys.emplace_back(key);
+  }
+  return keys;
+}
+
+template <typename F>
+[[nodiscard]] static double estimate_false_positive_rate(const F& fil) {
+  auto         gen         = std::mt19937_64(std::random_device{}());
+  size_t       matches     = 0;
+  const size_t sample_size = 1'000'000;
+  for (size_t t = 0; t < sample_size; t++) {
+    if (fil.contains(gen())) { // no distribution needed
+      matches++;
+    }
+  }
+  return static_cast<double>(matches) / static_cast<double>(sample_size) -
+         static_cast<double>(fil.size()) /
+             static_cast<double>(std::numeric_limits<std::uint64_t>::max());
+}
--- test/test_binfuse.cpp
+++ test/test_binfuse.cpp
@@ -1,99 +1,12 @@
 #include "binaryfusefilter.h"
 #include "binfuse/filter.hpp"
 #include "binfuse/sharded_filter.hpp"
+#include "helpers.hpp"
 #include "gtest/gtest.h"
-#include <charconv>
 #include <cstdint>
 #include <filesystem>
-#include <random>
 #include <stdexcept>
 #include <vector>
-
-std::vector<std::uint64_t> load_sample() {
-  std::ifstream              sample("data/sample.txt");
-  std::vector<std::uint64_t> keys;
-  for (std::string line; std::getline(sample, line);) {
-    std::uint64_t key{};
-    std::from_chars(line.data(), line.data() + line.size(), key, 16);
-    keys.emplace_back(key);
-  }
-  return keys;
-}
-
-template <binfuse::filter_type FilterType>
-void test_filter(std::span<const std::uint64_t> keys, double max_false_positive_rate) {
-  auto filter = binfuse::filter<FilterType>(keys);
-  EXPECT_TRUE(filter.verify(keys));
-  EXPECT_LE(estimate_false_positive_rate(filter), max_false_positive_rate);
-}
-
-template <binfuse::filter_type FilterType>
-void test_sharded_filter(std::span<const std::uint64_t> keys, double max_false_positive_rate,
-                         uint8_t sharded_bits = 8) {
-  std::filesystem::path filter_filename;
-  filter_filename = "tmp/sharded_filter.bin";
-  {
-    binfuse::sharded_filter<FilterType, mio::access_mode::write> sharded_sink(filter_filename,
-                                                                              sharded_bits);
-
-    sharded_sink.stream_prepare();
-    for (auto key: keys) {
-      sharded_sink.stream_add(key);
-    }
-    sharded_sink.stream_finalize();
-
-    const binfuse::sharded_filter<FilterType, mio::access_mode::read> sharded_source(
-        filter_filename, sharded_bits);
-
-    // full verify across all shards
-    for (auto needle: keys) {
-      EXPECT_TRUE(sharded_source.contains(needle));
-    }
-
-    EXPECT_LE(estimate_false_positive_rate(sharded_source), max_false_positive_rate);
-  } // allow mmap to destroy before removing file (required on windows)
-
-  std::filesystem::remove(filter_filename);
-}
-
-template <typename F>
-[[nodiscard]] static double estimate_false_positive_rate(const F& fil) {
-  auto         gen         = std::mt19937_64(std::random_device{}());
-  size_t       matches     = 0;
-  const size_t sample_size = 1'000'000;
-  for (size_t t = 0; t < sample_size; t++) {
-    if (fil.contains(gen())) { // no distribution needed
-      matches++;
-    }
-  }
-  return static_cast<double>(matches) / static_cast<double>(sample_size) -
-         static_cast<double>(fil.size()) /
-             static_cast<double>(std::numeric_limits<std::uint64_t>::max());
-}
-
-TEST(binfuse, filter8) { // NOLINT
-  test_filter<binary_fuse8_t>(load_sample(), 0.005);
-}
-
-TEST(binfuse, sharded_filter8) { // NOLINT
-  test_sharded_filter<binary_fuse8_t>(load_sample(), 0.005);
-}
-
-TEST(binfuse, filter16) { // NOLINT
-  test_filter<binary_fuse16_t>(load_sample(), 0.00005);
-}
-
-TEST(binfuse, sharded_filter16) { // NOLINT
-  test_sharded_filter<binary_fuse16_t>(load_sample(), 0.00005);
-}
-
-TEST(binfuse, sharded_filter8_32) {                             // NOLINT
-  test_sharded_filter<binary_fuse8_t>(load_sample(), 0.005, 5); // 5 sharded_bits
-}
-
-TEST(binfuse, sharded_filter16_32) {                               // NOLINT
-  test_sharded_filter<binary_fuse16_t>(load_sample(), 0.00005, 5); // 5 sharded_bits
-}
 
 TEST(binfuse_sharded_filter, default_construct) { // NOLINT
   binfuse::sharded_filter8_source sharded_source;
@@ -168,3 +81,66 @@ TEST(binfuse_sharded_filter, load_tiny) { // NOLINT
   EXPECT_TRUE(sharded_tiny_source.contains(0x0000000000000002));
   EXPECT_TRUE(sharded_tiny_source.contains(0x8000000000000000));
 }
+
+// larger data tests
+
+template <binfuse::filter_type FilterType>
+void test_filter(std::span<const std::uint64_t> keys, double max_false_positive_rate) {
+  auto filter = binfuse::filter<FilterType>(keys);
+  EXPECT_TRUE(filter.verify(keys));
+  EXPECT_LE(estimate_false_positive_rate(filter), max_false_positive_rate);
+}
+
+template <binfuse::filter_type FilterType>
+void test_sharded_filter(std::span<const std::uint64_t> keys, double max_false_positive_rate,
+                         uint8_t sharded_bits = 8) {
+  std::filesystem::path filter_filename;
+  filter_filename = "tmp/sharded_filter.bin";
+  {
+    binfuse::sharded_filter<FilterType, mio::access_mode::write> sharded_sink(filter_filename,
+                                                                              sharded_bits);
+
+    sharded_sink.stream_prepare();
+    for (auto key: keys) {
+      sharded_sink.stream_add(key);
+    }
+    sharded_sink.stream_finalize();
+
+    const binfuse::sharded_filter<FilterType, mio::access_mode::read> sharded_source(
+        filter_filename, sharded_bits);
+
+    // full verify across all shards
+    for (auto needle: keys) {
+      EXPECT_TRUE(sharded_source.contains(needle));
+    }
+
+    EXPECT_LE(estimate_false_positive_rate(sharded_source), max_false_positive_rate);
+  } // allow mmap to destroy before removing file (required on windows)
+
+  std::filesystem::remove(filter_filename);
+}
+
+TEST(binfuse, filter8) { // NOLINT
+  test_filter<binary_fuse8_t>(load_sample(), 0.005);
+}
+
+TEST(binfuse, sharded_filter8) { // NOLINT
+  test_sharded_filter<binary_fuse8_t>(load_sample(), 0.005);
+}
+
+TEST(binfuse, filter16) { // NOLINT
+  test_filter<binary_fuse16_t>(load_sample(), 0.00005);
+}
+
+TEST(binfuse, sharded_filter16) { // NOLINT
+  test_sharded_filter<binary_fuse16_t>(load_sample(), 0.00005);
+}
+
+TEST(binfuse, sharded_filter8_32) {                             // NOLINT
+  test_sharded_filter<binary_fuse8_t>(load_sample(), 0.005, 5); // 5 sharded_bits
+}
+
+TEST(binfuse, sharded_filter16_32) {                               // NOLINT
+  test_sharded_filter<binary_fuse16_t>(load_sample(), 0.00005, 5); // 5 sharded_bits
+}
+
