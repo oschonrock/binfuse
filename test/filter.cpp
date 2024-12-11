f30dfcfc36d5db7fdbeeff3cf2a31b753c9e9655
Oliver Schonrock <oliver@schonrocks.com>
2024-12-11 04:48:19 +0000

    remove superfluous test helper template

--- test/filter.cpp
+++ test/filter.cpp
@@ -85,17 +85,16 @@ TEST(binfuse_filter, move) { // NOLINT
 
 // larger data tests
 
-template <binfuse::filter_type FilterType>
-void test_filter(std::span<const std::uint64_t> keys, double max_false_positive_rate) {
-  auto filter = binfuse::filter<FilterType>(keys);
-  EXPECT_TRUE(filter.verify(keys));
-  EXPECT_LE(estimate_false_positive_rate(filter), max_false_positive_rate);
-}
-
 TEST(binfuse_filter, large8) { // NOLINT
-  test_filter<binary_fuse8_t>(load_sample(), 0.005);
+  auto keys= load_sample();
+  auto filter = binfuse::filter<binary_fuse8_t>(keys);
+  EXPECT_TRUE(filter.verify(keys));
+  EXPECT_LE(estimate_false_positive_rate(filter),  0.005);
 }
 
 TEST(binfuse_filter, large16) { // NOLINT
-  test_filter<binary_fuse16_t>(load_sample(), 0.00005);
+  auto keys= load_sample();
+  auto filter = binfuse::filter<binary_fuse16_t>(keys);
+  EXPECT_TRUE(filter.verify(keys));
+  EXPECT_LE(estimate_false_positive_rate(filter),  0.00005);
 }
