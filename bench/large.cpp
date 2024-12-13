db40f31904f1a38495fcd44d69e2c189388d453f
Oliver Schonrock <oliver@schonrocks.com>
2024-12-13 10:41:12 +0000

    centralise casting and give progress reports

--- bench/large.cpp
+++ bench/large.cpp
@@ -1,5 +1,6 @@
 #include "binfuse/sharded_filter.hpp"
 #include <chrono>
+#include <concepts>
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
@@ -9,8 +10,7 @@
 #include <stdexcept>
 #include <vector>
 
-using clk   = std::chrono::high_resolution_clock;
-using nanos = std::chrono::duration<double, std::nano>;
+using clk = std::chrono::high_resolution_clock;
 
 std::vector<std::uint64_t> gen_shard(std::uint64_t prefix, std::uint8_t shard_bits,
                                      std::size_t size) {
@@ -31,6 +31,15 @@ std::vector<std::uint64_t> gen_shard(std::uint64_t prefix, std::uint8_t shard_bi
   return shard;
 }
 
+double ratio(std::integral auto numerator, std::integral auto denominator) {
+  return static_cast<double>(numerator) / static_cast<double>(denominator);
+}
+
+double dratio(auto duration, std::integral auto denominator) {
+  using nanos = std::chrono::duration<double, std::nano>;
+  return duration_cast<nanos>(duration).count() / static_cast<double>(denominator);
+}
+
 template <typename T>
 void populate(T& filter, std::uint32_t shards, std::uint8_t shard_bits, std::size_t shard_size) {
   clk::duration gen_shard_total{};
@@ -38,6 +47,7 @@ void populate(T& filter, std::uint32_t shards, std::uint8_t shard_bits, std::siz
   clk::duration verify_filter_total{};
   clk::duration add_shard_total{};
   for (std::uint32_t prefix = 0; prefix != shards; ++prefix) {
+    std::cerr << std::format("populate: {:6.1f}%\x1B[17D", 100 * ratio(prefix, shards));
     auto       start      = clk::now();
     const auto shard_keys = gen_shard(prefix, shard_bits, shard_size);
     gen_shard_total += clk::now() - start;
@@ -56,12 +66,11 @@ void populate(T& filter, std::uint32_t shards, std::uint8_t shard_bits, std::siz
     filter.add_shard(shard, prefix);
     add_shard_total += clk::now() - start;
   }
-  auto size = static_cast<double>(shards * shard_size);
-  std::cout << std::format("f{:<2d} {:8.1f}ns {:8.1f}ns {:8.1f}ns {:8.1f}ns", T::nbits,
-                           duration_cast<nanos>(gen_shard_total).count() / size,
-                           duration_cast<nanos>(popluate_filter_total).count() / size,
-                           duration_cast<nanos>(verify_filter_total).count() / size,
-                           duration_cast<nanos>(add_shard_total).count() / size);
+  auto size = shards * shard_size;
+  std::cout << std::format(
+      "f{:<2d} {:8.1f}ns {:8.1f}ns {:8.1f}ns {:8.1f}ns", T::nbits,
+      dratio(gen_shard_total, size), dratio(popluate_filter_total, size),
+      dratio(verify_filter_total, size), dratio(add_shard_total, size));
 }
 
 template <typename T>
@@ -70,13 +79,16 @@ void query(const T& filter, std::size_t size) {
 
   auto        start       = clk::now();
   std::size_t found_count = 0;
-  for (std::size_t i = 0; i != size; ++i) {
+  auto        iterations  = std::min(size, 1'000'000UL);
+  for (std::size_t i = 0; i != iterations; ++i) {
+    if (i % (iterations / 4000) == 0) {
+      std::cerr << std::format(" query: {:6.1f}%\x1B[15D", 100 * ratio(i, iterations));
+    }
     found_count += filter.contains(gen());
   }
   auto end = clk::now();
-  std::cout << std::format(" {:8.1f}ns  {:.6f}%\n",
-                           duration_cast<nanos>(end - start).count() / static_cast<double>(size),
-                           100.0 * static_cast<double>(found_count) / static_cast<double>(size));
+  std::cout << std::format(" {:8.1f}ns  {:.6f}%\n", dratio(end - start, iterations),
+                           100 * ratio(found_count, iterations));
 }
 
 int main() {
